9998b8170a13bce6460ba456bde3573a3119d9ef
Dave Coykendall <Relic180@users.noreply.github.com>
2024-10-31 13:43:15 -0600

    Modifications to commands to acomidate sudo pm2 interactions

--- commands.sh
+++ commands.sh
@@ -5,46 +5,51 @@ set -e  # Exit immediately if a command exits with a non-zero status
 TARGET=$1
 BRANCH=$2
 
+# Debugging Information
 env > /tmp/webhook_env.log # DEBUGGING
 echo "Node version: $(node -v)" # DEBUGGING
-
 echo "Starting deployment for $TARGET on branch $BRANCH" # DEBUGGING
 echo "User: $(whoami)" # DEBUGGING
 echo "Home Directory: $HOME" # DEBUGGING
 echo "Current Directory: $(pwd)" # DEBUGGING
-echo "Node Version: $(node -v)" # DEBUGGING
-echo "NPM Version: $(npm -v)" # DEBUGGING
 echo "PATH: $PATH" # DEBUGGING
 
-case $TARGET in
-  crackin)
-    cd /home/relic/web/crackin.com/app
+case "$TARGET" in
+  webhooks|crackin|rentalguru)
+    sudo /home/relic/web/pm2_actions.sh stop "$TARGET"
+
+    case "$TARGET" in
+      webhooks)
+        cd /home/relic/web/webhooks
+        ;;
+      crackin)
+        cd /home/relic/web/crackin.com/app
+        ;;
+      rentalguru)
+        cd /home/relic/web/my.rentalguru.ai/app
+        ;;
+      *)
+        echo "Invalid application name: $TARGET"
+        exit 1
+        ;;
+    esac
 
     # Ensure we're on the correct branch
-    git fetch origin $BRANCH
-    git reset --hard origin/$BRANCH
-    pm2 stop crackin
+    git fetch origin "$BRANCH"
+    git reset --hard "origin/$BRANCH"
 
     # Remove existing node_modules
     rm -rf node_modules
 
     npm install
     npm run build
-    pm2 restart crackin
-    ;;
-  rentalguru)
-    cd /home/relic/web/my.rentalguru.ai/app
-    git checkout --quiet $BRANCH
-    git pull origin $BRANCH
-    npm install
-    npm run build
-    pm2 restart rentalGuru
+    sudo /home/relic/web/pm2_actions.sh start "$TARGET"
     ;;
   missioncrit)
     cd /home/relic/web/missioncritical.us.com/repo
 
-    git fetch origin $BRANCH
-    git reset --hard origin/$BRANCH
+    git fetch origin "$BRANCH"
+    git reset --hard "origin/$BRANCH"
 
     cp -a ./missioncritical/. ../public_html/
     cp -a ./jrsupply/. ../../jrsupply.us.com/public_html/
